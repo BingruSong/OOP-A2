c59eb6ffd18d07a82e5eb30d910060a4eb6a33d1
BingruSong <s13836733471@163.com>
2025-11-30 10:10:00 +0800

    Interface

---
+++ BingruSong-A2/RideInterface.java
@@ -0,0 +1,16 @@
+
+public interface RideInterface {
+    // Queue management methods
+    void addVisitorToQueue(Visitor visitor);
+    void removeVisitorFromQueue();
+    void printQueue();
+    
+    // Ride history methods
+    void addVisitorToHistory(Visitor visitor);
+    boolean checkVisitorFromHistory(Visitor visitor);
+    int numberOfVisitors();
+    void printRideHistory();
+    
+    // Ride operation method
+    void runOneCycle();
+}
